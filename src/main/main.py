19724ff9d86680508ac7f9f1db5d17eb1f9f1cac
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-03-11 23:45:50 -0300

    add routes

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
+
+from src.adapters.controllers.check_token_controller import CheckTokenController
+from src.adapters.controllers.create_user_controller import CreateUserController
+from src.adapters.controllers.delete_user_controller import DeleteUserController
 from src.adapters.controllers.get_all_users_controller import GetAllUsersController
 from src.adapters.controllers.get_user_by_cpfrne_controller import GetUserByCpfRneController
+from src.adapters.controllers.login_user_controller import LoginUserController
+from src.adapters.controllers.update_user_controller import UpdateUserController
 from src.adapters.errors.http_exception import HttpException
 from src.adapters.helpers.http_models import HttpRequest
 from src.main.users.module import Modular
@@ -21,13 +27,12 @@ app.add_middleware(
 async def internal_exception_handler(request: Request, exc: HttpException):
     return PlainTextResponse(exc.body, status_code=exc.status_code)
 
-@app.get("/")
+@app.get("/user/all")
 async def getAllusers(response: Response):
     getAllUsersController = Modular.getInject(GetAllUsersController)
     req = HttpRequest(query=None)
     result = await getAllUsersController(req)
-    response.status_code = status.get(result.status_code)
-    return result.body
+    return result
 
 
 @app.get("/user/{cpfRne}")
@@ -39,4 +44,41 @@ async def geUser(cpfRne: int, response: Response):
     result = await getAllUserByCpfRne(req)
     response.status_code = status.get(result.status_code)
     return result.body
+
+@app.post("/user")
+async def root(request: Request):
+    createUserController = Modular.getInject(CreateUserController)
+    req = HttpRequest(body=await request.json())
+    result = await createUserController(req)
+    return result
+
+@app.put("/user")
+async def root(request: Request):
+    updateUserController = Modular.getInject(UpdateUserController)
+    req = HttpRequest(body=await request.json())
+    result = await updateUserController(req)
+    return result
+
+@app.delete("/user")
+async def root(request: Request):
+    deleteUserController = Modular.getInject(DeleteUserController)
+    req = HttpRequest(body=await request.json())
+    result = await deleteUserController(req)
+    return result
+
+@app.post("/login")
+async def login(request: Request):
+    loginUserController = Modular.getInject(LoginUserController)
+    req = HttpRequest(body=await request.json())
+    result = await loginUserController(req)
+    return result
+
+@app.post("/checkToken")
+async def checkToken(request: Request):
+    checkTokenController = Modular.getInject(CheckTokenController)
+    req = HttpRequest(body=await request.json())
+    result = await checkTokenController(req)
+    return result
+
+
 
--- src/main/users/module.py
+++ src/main/users/module.py
@@ -1,8 +1,13 @@
 
 from typing import Any
 
+from src.adapters.controllers.check_token_controller import CheckTokenController
+from src.adapters.controllers.create_user_controller import CreateUserController
+from src.adapters.controllers.delete_user_controller import DeleteUserController
 from src.adapters.controllers.get_all_users_controller import GetAllUsersController
 from src.adapters.controllers.get_user_by_cpfrne_controller import GetUserByCpfRneController
+from src.adapters.controllers.login_user_controller import LoginUserController
+from src.adapters.controllers.update_user_controller import UpdateUserController
 from src.domain.usecases.get_all_users_usecase import GetAllUsersUsecase
 from src.infra.repositories.cognito_repository import UserRepositoryCognito
 from src.infra.repositories.user_repository_mock import UserRepositoryMock
@@ -37,10 +42,14 @@ class Module:
     def getBinds():
         return [
             GetAllUsersController,
-            GetAllUsersUsecase,
             GetUserByCpfRneController,
-            UserRepositoryCognito
-            # UserRepositoryMock
+            CheckTokenController,
+            CreateUserController,
+            DeleteUserController,
+            LoginUserController,
+            UpdateUserController,
+            # UserRepositoryCognito
+            UserRepositoryMock
         ]
 
 
