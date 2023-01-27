fe200761372c374e1985368c22fd8565f8888fb5
Vitor Soller <vgsoller@gmail.com>
2023-01-27 17:38:28 -0300

    add get_user_viewmodel.py with UT

--- src/modules/get_user/app/get_user_viewmodel.py
+++ src/modules/get_user/app/get_user_viewmodel.py
@@ -1,19 +1,44 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
-from src.domain.entities.enums import ROLE, ACCESS_LEVEL
+from src.shared.domain.entities.enums import ROLE, ACCESS_LEVEL
+from src.shared.domain.entities.user import User
 
 
-class GetUserModel(BaseModel):
+class UserViewmodel:
     name: str
-    cpfRne: int
-    email: Optional[str]
-    ra: Optional[int]
+    email: str
+    ra: str
     role: ROLE
-    accessLevel: ACCESS_LEVEL
-    socialName: Optional[str]
+    access_level: ACCESS_LEVEL
+    social_name: str
+
+    def __init__(self, user: User):
+        self.user_id = user.user_id
+        self.name = user.name
+        self.email = user.email
+        self.ra = user.ra
+        self.role = user.role
+        self.access_level = user.access_level
+        self.social_name = user.social_name
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'name': self.name,
+            'email': self.email,
+            'ra': self.ra,
+            'role': self.role.value,
+            'access_level': self.access_level.value,
+            'social_name': self.social_name
+        }
 
 
+class GetUserViewmodel:
+    user: UserViewmodel
 
+    def __init__(self, user: User):
+        self.user = UserViewmodel(user)
 
+    def to_dict(self):
+        return {
+            'user': self.user.to_dict(),
+            'message': 'the user was retrieved'
+        }
