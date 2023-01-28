bbf4fe44dbea5d61678d1c9c253e427bcfbdc1d2
Vitor Soller <vgsoller@gmail.com>
2023-01-28 18:55:52 -0300

    fix interface

--- src/shared/domain/repositories/user_repository_interface.py
+++ src/shared/domain/repositories/user_repository_interface.py
@@ -6,47 +6,47 @@ from src.shared.domain.entities.user import User
 class IUserRepository(ABC):
 
     @abstractmethod
-    async def get_user_by_email(self, email: str) -> User:
+    def get_user_by_email(self, email: str) -> User:
         pass
 
     @abstractmethod
-    async def get_all_users(self) -> List[User]:
+    def get_all_users(self) -> List[User]:
         pass
 
     @abstractmethod
-    async def create_user(self, user: User) -> int:
+    def create_user(self, user: User) -> int:
         pass
 
     @abstractmethod
-    async def update_user(self, user: User) -> User:
+    def update_user(self, user: User) -> User:
         pass
 
     @abstractmethod
-    async def delete_user(self, cpfRne: int):
+    def delete_user(self, cpfRne: int):
         pass
 
     @abstractmethod
-    async def confirm_user_creation(self, login: str, code: int):
+    def confirm_user_creation(self, login: str, code: int):
         pass
 
     @abstractmethod
     # accessToken, refreshToken #todo change to login
-    async def login_user(self, login: str, password: str) -> dict:
+    def login_user(self, login: str, password: str) -> dict:
         pass
 
     @abstractmethod
-    async def check_token(self, token: str) -> dict:  # user data
+    def check_token(self, token: str) -> dict:  # user data
         pass
 
     @abstractmethod
     # accessToken, refreshToken
-    async def refresh_token(self, refreshToken: str) -> Tuple[str, str]:
+    def refresh_token(self, refreshToken: str) -> Tuple[str, str]:
         pass
 
     @abstractmethod
-    async def change_password(self, login: str) -> bool:
+    def change_password(self, login: str) -> bool:
         pass
 
     @abstractmethod
-    async def confirm_change_password(self, login: str, newPassword: str, code: str) -> bool:
+    def confirm_change_password(self, login: str, newPassword: str, code: str) -> bool:
         pass
