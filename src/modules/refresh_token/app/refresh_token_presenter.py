56b2e75195359929ab43bc5e6ec4082ef26f7df8
Vitor Soller <vgsoller@gmail.com>
2023-01-29 19:03:31 -0300

    add refresh_token_presenter.py with UT

---
+++ src/modules/refresh_token/app/refresh_token_presenter.py
@@ -0,0 +1,18 @@
+from .refresh_token_controller import RefreshTokenController
+from .refresh_token_usecase import RefreshTokenUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+
+repo = Environments.get_user_repo()()
+usecase = RefreshTokenUsecase(repo)
+controller = RefreshTokenController(usecase)
+
+def lambda_handler(event, context):
+
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
+
