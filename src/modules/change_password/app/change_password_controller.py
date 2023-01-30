d8ef80f080c60e1e9ebe42f407214b5e42a80129
Caio Toledo <caio@Caios-MacBook-Air.local>
2023-01-30 14:56:14 -0500

    changing errors and variable names

--- src/modules/change_password/app/change_password_controller.py
+++ src/modules/change_password/app/change_password_controller.py
@@ -1,52 +1,45 @@
-from src.shared.errors.http_exception import HttpException
-from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
-from src.shared.helpers.external_interfaces.http_models import HttpRequest, HttpResponse
-from src.shared.helpers.external_interfaces.http_codes import BadRequest, InternalServerError, OK
-from src.modules.change_password.app.change_password_viewmodel import ChangePasswordModel
-from src.modules.login_user.app.login_user_viewmodel import LoginUserModel
-from src.shared.domain.errors.errors import UnexpectedError, EntityError, NonExistentUser, InvalidCredentials, UserNotConfirmed
-from src.shared.domain.repositories.user_repository_interface import IUserRepository
 from src.modules.change_password.app.change_password_usecase import ChangePasswordUsecase
-from src.modules.login_user.app.login_user_usecase import LoginUserUsecase
+from src.modules.change_password.app.change_password_viewmodel import ChangePasswordViewmodel
+
+from src.shared.helpers.errors.controller_errors import MissingParameters
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
+from src.shared.helpers.external_interfaces.http_codes import OK, NotFound, BadRequest, InternalServerError
 
 
 class ChangePasswordController:
     def __init__(self, usecase: ChangePasswordUsecase):
-        self._changePasswordUsecase = usecase
+        self.ChangePasswordUsecase = usecase
 
-    async def __call__(self, req: IRequest) -> IResponse:
+    def __call__(self, request: IRequest) -> IResponse:
 
-        if req.data is not None:
-            return BadRequest('No parameters allowed.')
-        if not req.data:
+        if not request.data:
             return BadRequest('Missing body.')
-
         try:
-            cleanLogin = str(req.body["login"]).replace(' ', '')
-            result = await self._changePasswordUsecase(cleanLogin)
-            changePasswordModel = ChangePasswordModel(result=result)
-            if not result:
-                changePasswordModel.message = "User not found"
-                return BadRequest(changePasswordModel.toDict())
+            if request.data.get('email') is None:
+                raise MissingParameters('email')
 
-            return OK(changePasswordModel.toDict())
+            resp = self.ChangePasswordUsecase(
+                email=request.data.get('email')
+            )
 
-        except KeyError as err:
-            return BadRequest('Missing parameter: ' + err.args[0])
+            viewmodel = ChangePasswordViewmodel(resp)
 
-        except UnexpectedError as err:
-            err = InternalServerError(body=err.message)
-            return err
+            return OK(viewmodel.to_dict())
 
-        except NonExistentUser as err:
-            err = BadRequest(body=err.message + " or is not confirmed")
-            return err
+        except NoItemsFound as err:
 
-        except UserNotConfirmed as err:
-            err = BadRequest(body=err.message)
-            err.status_code = 401
-            return err
+            return NotFound(body=err.message)
+
+        except MissingParameters as err:
+
+            return BadRequest(body=err.message)
+
+        except EntityError as err:
+
+            return BadRequest(body=err.message)
 
         except Exception as err:
-            err = InternalServerError(body=err.args[0])
-            return err
+
+            return InternalServerError(body=err.args[0])
