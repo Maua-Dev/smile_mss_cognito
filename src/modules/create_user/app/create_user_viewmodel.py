62decd14d96743c1e9e47dde146fd78ee61b33e6
Vitor Soller <vgsoller@gmail.com>
2023-01-31 18:18:34 -0300

    add create_user_viewmodel.py

---
+++ src/modules/create_user/app/create_user_viewmodel.py
@@ -0,0 +1,47 @@
+from src.shared.domain.entities.enums import ROLE, ACCESS_LEVEL
+from src.shared.domain.entities.user import User
+
+
+class UserViewmodel:
+    name: str
+    email: str
+    ra: str
+    role: ROLE
+    access_level: ACCESS_LEVEL
+    social_name: str
+    phone: str
+
+    def __init__(self, user: User):
+        self.user_id = user.user_id
+        self.name = user.name
+        self.email = user.email
+        self.ra = user.ra
+        self.role = user.role
+        self.access_level = user.access_level
+        self.social_name = user.social_name
+        self.phone = user.phone
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'name': self.name,
+            'email': self.email,
+            'ra': self.ra,
+            'role': self.role.value,
+            'access_level': self.access_level.value,
+            'social_name': self.social_name,
+            'phone': self.phone,
+        }
+
+
+class CreateUserViewmodel:
+    user: UserViewmodel
+
+    def __init__(self, user: User):
+        self.user = UserViewmodel(user)
+
+    def to_dict(self):
+        return {
+            'user': self.user.to_dict(),
+            'message': 'the user was created'
+        }
