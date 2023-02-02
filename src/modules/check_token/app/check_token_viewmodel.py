6aa8c1fa24edcb4a92a0d9e2d3d66d1fd13c075e
Caio Toledo <caio@Caios-MacBook-Air.local>
2023-02-02 00:10:33 -0300

    changing viewmodel to pattern

--- src/modules/check_token/app/check_token_viewmodel.py
+++ src/modules/check_token/app/check_token_viewmodel.py
@@ -1,38 +1,35 @@
 from src.shared.domain.entities.enums import ROLE, ACCESS_LEVEL
 
 
-class CheckTokenModel():
+class CheckTokenViewmodel():
     role: ROLE
-    accesslevel: ACCESS_LEVEL
-    cpfRne: int
+    access_level: ACCESS_LEVEL
     email: str
-    validToken: bool
+    user_id: str
+    valid_token: bool
 
-    def __init__(self, role: str, accesslevel: str, cpfRne: int, email: str, validToken: bool, id: str = None):
+    def __init__(self, role: ROLE, access_level: ACCESS_LEVEL, email: str, valid_token: bool, user_id: str = None):
         self.role = role
-        self.accesslevel = accesslevel
-        self.cpfRne = cpfRne
+        self.access_level = access_level
         self.email = email
-        self.validToken = validToken
-        self.id = id
+        self.valid_token = valid_token
+        self.user_id = user_id
 
     @staticmethod
-    def fromDict(data: dict):
-        return CheckTokenModel(
+    def from_dict(data: dict):
+        return CheckTokenViewmodel(
             role=data['role'],
-            accesslevel=data['accessLevel'],
-            cpfRne=data['cpfRne'],
+            access_level=data['access_level'],
             email=data['email'],
-            validToken=data['validToken'],
-            id=data['id']
+            valid_token=data['valid_token'],
+            user_id=data['user_id']
         )
 
-    def toDict(self):
+    def to_dict(self):
         return {
-            'role': self.role.value,
-            'access_level': self.accesslevel.value,
-            'cpf_rne': self.cpfRne,
+            'role': self.role,
+            'access_level': self.access_level,
             'email': self.email,
-            'valid_token': self.validToken,
-            'id': self.id if self.id else None
+            'valid_token': self.valid_token,
+            'user_id': self.user_id if self.user_id else None
         }
