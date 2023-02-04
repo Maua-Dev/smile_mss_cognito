e692fb383a17e2a84c35660fd231625724811bd4
Caio Toledo <caio@Caios-MacBook-Air.local>
2023-02-03 23:06:07 -0300

    changing confirm user creation usecase

--- src/modules/confirm_user_creation/app/confirm_user_creation_usecase.py
+++ src/modules/confirm_user_creation/app/confirm_user_creation_usecase.py
@@ -1,13 +1,21 @@
 
-from src.domain.repositories.user_repository_interface import IUserRepository
+from src.shared.domain.entities.user import User
+from src.shared.domain.repositories.user_repository_interface import IUserRepository
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction
 
 
 class ConfirmUserCreationUsecase:
 
-    def __init__(self, userRepository: IUserRepository):
-        self._userRepository = userRepository
+    def __init__(self, repo: IUserRepository):
+        self.repo = repo
 
-    async def __call__(self, login: str, code: str) -> bool:
-        result = await self._userRepository.confirmUserCreation(login, code)
+    def __call__(self, email: str, confirmation_code: str) -> bool:
+        user = self.repo.get_unconfirmed_user_by_email(email)
+        if not user:
+            raise ForbiddenAction('"User not found".')
+        if user in self.repo.get_confirmed_users():
+            raise ForbiddenAction('"User already confirmed".')
+
+        result = self.repo.confirm_user_creation(email, confirmation_code)
+
         return result
-
