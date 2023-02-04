7251664650ed02542b24e28884671764c89791c4
Caio Toledo <caio@Caios-MacBook-Air.local>
2023-02-04 00:03:30 -0300

    changing delete user usecase to new pattern

--- src/modules/delete_user/app/delete_user_usecase.py
+++ src/modules/delete_user/app/delete_user_usecase.py
@@ -1,20 +1,19 @@
 from src.shared.domain.entities.user import User
 from src.shared.domain.repositories.user_repository_interface import IUserRepository
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction
 
 
 class DeleteUserUsecase:
 
-    def __init__(self, userRepository: IUserRepository):
-        self._userRepository = userRepository
+    def __init__(self, repo: IUserRepository):
+        self.repo = repo
 
-    async def __call__(self, cpfRne: int):
-        try:
-            # Check if immutable fields are changed
-            getUserByCpfRneUsecase = GetUserByCpfRneUsecase(
-                self._userRepository)
-            await getUserByCpfRneUsecase(cpfRne)
+    def __call__(self, email: int):
 
-            await self._userRepository.deleteUser(cpfRne)
+        user = self.repo.get_user_by_email(email)
+        if not user:
+            raise ForbiddenAction('"User not found"')
 
-        except (NoItemsFound, NonExistentUser) as error:
-            raise NonExistentUser(error.message)
+        self.repo.delete_user(email)
+
+        return True
