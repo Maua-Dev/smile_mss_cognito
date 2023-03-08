ed8f0feab43c18f233006f3b525555099f815f4b
Vitor Soller <vgsoller@gmail.com>
2023-03-08 20:32:37 -0300

    fix admin creation ⚪⬜🦕🗜🦾

--- src/modules/create_user/app/create_user_usecase.py
+++ src/modules/create_user/app/create_user_usecase.py
@@ -1,8 +1,7 @@
-from src.shared.domain.entities.enums import ACCESS_LEVEL
+from src.shared.domain.entities.enums import ACCESS_LEVEL, ROLE
 from src.shared.domain.entities.user import User
 from src.shared.domain.repositories.user_repository_interface import IUserRepository
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import ForbiddenAction
 
 
 class CreateUserUsecase:
@@ -13,6 +12,9 @@ class CreateUserUsecase:
     def __call__(self, user: User) -> User:
 
         if user.access_level != ACCESS_LEVEL.USER:
+            raise EntityError('access_level')
+
+        if user.role == ROLE.ADMIN:
             raise EntityError('access_level')
 
         user.email = user.email.lower()
