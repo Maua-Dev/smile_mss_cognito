8eda7914e3355b273c522361418f742259282682
Vitor Soller <vgsoller@gmail.com>
2023-03-28 22:02:32 -0300

    remove test

--- tests/modules/create_user/app/test_create_user_controller.py
+++ tests/modules/create_user/app/test_create_user_controller.py
@@ -179,27 +179,27 @@ class Test_CreateUserController:
         assert response.status_code == 400
         assert response.body == 'Parâmetro ausente: accepted_terms'
 
-    def test_create_user_controller_missing_accepted_notifications_sms(self):
-        repo = UserRepositoryMock()
-        usease = CreateUserUsecase(repo)
-        controller = CreateUserController(usease)
-        request = HttpRequest(body={
-            'email': 'vitor@gmail.com',
-            'name': 'Vitor Soller',
-            'password': '123456',
-            'ra': '21014442',
-            'role': 'STUDENT',
-            'access_level': 'USER',
-            'accepted_terms': True,
-            'accepted_notifications_email': True,
-            'certificate_with_social_name': False,
-            'phone': '+5511991758098'
-        })
-
-        response = controller(request)
-
-        assert response.status_code == 400
-        assert response.body == 'Parâmetro ausente: accepted_notifications_sms'
+    # def test_create_user_controller_missing_accepted_notifications_sms(self):
+    #     repo = UserRepositoryMock()
+    #     usease = CreateUserUsecase(repo)
+    #     controller = CreateUserController(usease)
+    #     request = HttpRequest(body={
+    #         'email': 'vitor@gmail.com',
+    #         'name': 'Vitor Soller',
+    #         'password': '123456',
+    #         'ra': '21014442',
+    #         'role': 'STUDENT',
+    #         'access_level': 'USER',
+    #         'accepted_terms': True,
+    #         'accepted_notifications_email': True,
+    #         'certificate_with_social_name': False,
+    #         'phone': '+5511991758098'
+    #     })
+    #
+    #     response = controller(request)
+    #
+    #     assert response.status_code == 400
+    #     assert response.body == 'Parâmetro ausente: accepted_notifications_sms'
 
     def test_create_user_controller_missing_accepted_notifications_email(self):
         repo = UserRepositoryMock()
