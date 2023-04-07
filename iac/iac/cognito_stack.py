2baa81389528bad44f31faef5f97016ffddc2993
Vitor Soller <vgsoller@gmail.com>
2023-04-07 13:24:06 -0300

    change email from name 😀

--- iac/iac/cognito_stack.py
+++ iac/iac/cognito_stack.py
@@ -19,7 +19,6 @@ class CognitoStack(Construct):
         self.email = aws_cognito.UserPoolEmail.with_ses(
             ses_region=os.environ.get("SES_EMAIL"),
             from_email=os.environ.get("FROM_EMAIL"),
-            from_name="SMILE 2023",
         )
 
         self.user_pool = aws_cognito.UserPool(self, "smile_user_pool",
