8adca2bdfd0d6a53c95e8aa2bc78ec0355d0771b
Vitor Soller <vgsoller@gmail.com>
2023-05-01 10:42:57 -0300

    fix list_professor_controller.py 😎

--- tests/modules/list_professors/app/test_list_professors_controller.py
+++ tests/modules/list_professors/app/test_list_professors_controller.py
@@ -13,10 +13,73 @@ class Test_ListProfessorsController:
         usecase = ListProfessorsUsecase(repo, observability=observability)
         controller = ListProfessorsController(usecase, observability=observability)
 
-        request = HttpRequest()
+        request = HttpRequest(
+            headers={'Authorization': 'Bearer ' + 'valid_access_token-' + repo.confirmed_users[1].email}
+        )
 
         list_professors = controller(request=request)
 
         assert list_professors.status_code == 200
         assert list_professors.body['message'] == 'the professors were retrieved'
+
+    def test_list_professors_missing_access_token(self):
+        repo = UserRepositoryMock()
+        usecase = ListProfessorsUsecase(repo, observability=observability)
+        controller = ListProfessorsController(usecase, observability=observability)
+
+        request = HttpRequest(
+            headers={}
+        )
+
+        list_professors = controller(request=request)
+
+        assert list_professors.status_code == 400
+        assert list_professors.body == f'Parâmetros ausentes: Authorization header'
+
+    def test_list_professors_invalid_access_token(self):
+        repo = UserRepositoryMock()
+        usecase = ListProfessorsUsecase(repo, observability=observability)
+        controller = ListProfessorsController(usecase, observability=observability)
+
+        request = HttpRequest(
+            headers={'Authorization': 'Bearer ' + 'invalid_access_token'}
+        )
+
+        list_professors = controller(request=request)
+
+        assert list_professors.status_code == 400
+        assert list_professors.body == f'Parâmetro inválido: access_token'
+
+    def test_list_professor_not_found(self):
+        repo = UserRepositoryMock()
+        usecase = ListProfessorsUsecase(repo, observability=observability)
+        controller = ListProfessorsController(usecase, observability=observability)
+
+        request = HttpRequest(
+            headers={'Authorization': 'Bearer ' + 'valid_access_token-emailfalso@maua.br'}
+        )
+
+        list_professors = controller(request=request)
+
+        assert list_professors.status_code == 404
+        assert list_professors.body == f'Usuário não encontrado'
+
+    def test_list_professors_not_admin(self):
+        repo = UserRepositoryMock()
+        usecase = ListProfessorsUsecase(repo, observability=observability)
+        controller = ListProfessorsController(usecase, observability=observability)
+
+        request = HttpRequest(
+            headers={'Authorization': 'Bearer ' + 'valid_access_token-zeeba@gmail.com'}
+        )
+
+        list_professors = controller(request=request)
+
+        assert list_professors.status_code == 403
+        assert list_professors.body == f'Apenas administradores podem acessar essa funcionalidade'
+
+
+
+
+
 
